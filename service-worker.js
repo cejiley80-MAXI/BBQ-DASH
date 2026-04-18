3801e9994f14202d089f0f843fba608af2619463
cejiley80-MAXI <cejiley80@gmail.com>
2026-04-17 19:43:08 -0600

    Primer commit del dashboard PWA

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'cesar-bbq-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => key !== CACHE_NAME && caches.delete(key))
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
